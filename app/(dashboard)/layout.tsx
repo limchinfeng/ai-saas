97f12493f6a666d94826814700e1917bc6ab140e
supershuaifeng <limchinfeng@gmail.com>
2023-08-25 16:20:30 +0800

    Done AI limit counter

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,18 +1,23 @@
 import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
+import { getApiLimitCount } from "@/lib/api-limit";
 
-const DashboardLayout = ({
+const DashboardLayout = async ({
     children
 }: {
     children: React.ReactNode;
 }) => {
+
+    // const apiLimitCount = await getApiLimitCount();
+    const apiLimitCount = 1;
+
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:flex-col md:w-72
                 md:fixed md:inset-y-0 z-[80] bg-gray-900 text-white
             ">
                 <div>
-                    <Sidebar />
+                    <Sidebar apiLimitCount={apiLimitCount} />
                 </div>
             </div>
             <main className="md:pl-72">
